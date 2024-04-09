ca10a12e579d56b7da39b8eb8223fa02f84d5c03
Kaiwen Wu <kwwu2015@gmail.com>
2024-04-08 21:04:44 -0400

    a batched implementation, need debug

--- main.py
+++ main.py
@@ -4,60 +4,98 @@ import numpy as np
 import torch
 
 
-class AngleSampler(object):
-    def __init__(self, A, b, x, nu):
+class EllipticalSliceSampler(object):
+    def __init__(self, A, b, x, z):
         """
         Linear inequality constraints
             A @ x + b <= 0.
 
         Args:
-            A: tensor
-            b: tensor
-            x: tensor, has to satisfies the linear inequality constraints
-            nu: tensor
+            A (tensor): the matrix of size (m, d) representing the constraint
+            b (tensor): the vector of size (d,) representing the constraint
+            x (tensor): a point on ellipse of size (*, d). Has to satisfy the linear inequality constraints
+            z (tensor): another point on the ellipse of size (*, d)
         """
-        if not A.matmul(x).add(b).le(0.).all():
+        self.A = A
+        self.b = b
+
+        self.x = x = x.unsqueeze(-2) if x.dim() == 1 else x
+        self.z = z = z.unsqueeze(-2) if z.dim() == 1 else z
+
+        if (x @ A.T + b).gt(0.).any():
             raise RuntimeError
 
-        angles = self.feasible_angles(A @ x, A @ nu, b)
+        self.one_to_batch = torch.arange(x.size(-2), dtype=torch.int64, device=x.device)
 
-        if not angles.size % 2 == 0:
-            raise RuntimeError
+        angles = self.intersection_angles(x @ A.T, z @ A.T, b.unsqueeze(-2))
 
-        # each row of self.slices represents an interval [a, b]
-        self.slices = angles.reshape(-1, 2)
-        print(self.slices)
+        self.right_endpoints, self.left_endpoints = self.candidate_endpoints(angles)
+        self.csum = self.right_endpoints.sub(self.left_endpoints).clamp(min=0.).cumsum(dim=-1)
 
-        self.csum = np.cumsum(self.slices[:, 1] - self.slices[:, 0])
+        # print("right endpoints\n", self.right_endpoints)
+        # print("left  endpoints\n", self.left_endpoints)
 
-    def sample(self):
-        u = self.csum[-1] * np.random.rand()
-        idx = np.searchsorted(self.csum, u)
-        return self.slices[idx, 0] + u - (self.csum[idx - 1] if idx > 0 else 0.)
+        # print("csum\n", self.csum)
 
-    def feasible_angles(self, p, q, bias):
+    def sample_angle(self):
+        u = self.csum[:, -1] * torch.rand(self.right_endpoints.size(-2))
+        # print(u)
+
+        idx = torch.searchsorted(self.csum, u.unsqueeze(-1)).squeeze(-1)
+        idx_gt_zero = idx > 0
+
+        increment = u
+        increment[idx_gt_zero] -= self.csum[idx_gt_zero, idx[idx_gt_zero] - 1]
+
+        return self.left_endpoints[self.one_to_batch, idx] + increment
+
+    def intersection_angles(self, p, q, r):
         """
         Solve the trigonometry inequalities
-            p * cos(theta) + q * sin(theta) + bias <= 0.
+            p * cos(theta) + q * sin(theta) + r <= 0.
+
+        Args:
+            p (tensor): of size (batch, m)
+            q (tensor): of size (batch, m)
+            r (tensor): broadcastable with p and q
         """
         normalize = torch.sqrt(p ** 2 + q ** 2)
 
+        if normalize.abs().lt(1e-10).any():
+            raise RuntimeError
+
         # It's impossible that the ratio < -1 if A @ x + b <= 0.
-        # Thus, () <= 1 is equivalent to abs() <= 1.
-        has_solution = -bias / normalize <= 1.
+        assert (-r / normalize).ge(-1).all()
 
-        if not has_solution.any():
-            return np.array([0., 2 * math.pi])
-
-        p, q, bias, normalize = (
-            p[has_solution], q[has_solution], bias[has_solution], normalize[has_solution]
-        )
-
-        arccos = torch.arccos(-1. * bias / normalize)
+        arccos = torch.arccos(-1. * r / normalize)
         arctan = torch.arctan(q / (normalize + p))
 
         theta1 = -1. * arccos + 2. * arctan
         theta2 = +1. * arccos + 2. * arctan
+
+        # print(theta1)
+        # print(theta2)
+        return theta1, theta2
+
+    def candidate_endpoints(self, angles):
+        """
+        Construct endpoints of active elliptical slices from intersection angles.
+
+        Args:
+            angles (tuple): angles[0] and angles[1] are intersection angles of size (batch, m)
+
+        Return:
+            candidate endpoints
+        """
+        theta1, theta2 = angles
+
+        if not torch.equal(theta1.isnan(), theta2.isnan()):
+            raise RuntimeError
+
+        is_nan = theta1.isnan()
+
+        theta1[is_nan] = math.pi
+        theta2[is_nan] = math.pi
 
         # translate every angle to [0, 2 * pi]
         theta1 = theta1 + theta1.lt(0.) * 2. * math.pi
@@ -65,22 +103,13 @@ class AngleSampler(object):
 
         theta1, theta2 = torch.minimum(theta1, theta2), torch.maximum(theta1, theta2)
 
-        sorted_one, indices_one = torch.sort(theta1, descending=False)
-        cummax = theta2[indices_one].cummax(dim=-1).values
-        keep_theta_one = sorted_one > torch.cat([torch.tensor([-1.]), cummax[:-1]], dim=-1)
+        srted, indices = torch.sort(theta1, descending=False)
+        cummax = theta2[self.one_to_batch.unsqueeze(-1), indices].cummax(dim=-1).values
 
-        sorted_two, indices_two = torch.sort(theta2, descending=True)
-        cummin = theta1[indices_two].cummin(dim=-1).values
-        keep_theta_two = sorted_two < torch.cat([torch.tensor([2 * math.pi + 1.]), cummin[:-1]], dim=-1)
-
-        # print(sorted_one[keep_theta_one])
-        # print(sorted_two[keep_theta_two])
-
-        unsorted_list = [0.] + [2. * math.pi] \
-            + sorted_one[keep_theta_one].tolist() \
-            + sorted_two[keep_theta_two].tolist()
-
-        return np.array(sorted(unsorted_list))
+        return (
+            torch.cat([srted, srted.new_ones(srted.size(-2), 1) * 2 * math.pi], dim=-1),
+            torch.cat([cummax.new_zeros(cummax.size(-2), 1), cummax], dim=-1),
+        )
 
 
 class TruncatedGaussianSampler(object):
@@ -89,12 +118,12 @@ class TruncatedGaussianSampler(object):
         self.b = b
 
     def next(self, x):
-        nu = torch.randn(x.size(-1))
+        z = torch.randn(x.size(-1))
 
-        sampler = AngleSampler(self.A, self.b, x, nu)
+        sampler = EllipticalSliceSampler(self.A, self.b, x, z)
 
-        theta = sampler.sample()
-        return x * math.cos(theta) + nu * math.sin(theta)
+        theta = sampler.sample_angle()
+        return x * torch.cos(theta).unsqueeze(-1) + z * torch.sin(theta).unsqueeze(-1)
 
 
 if __name__ == "__main__":
@@ -105,21 +134,18 @@ if __name__ == "__main__":
     A = torch.tensor([[-1.], [1.]])
     b = torch.tensor([-1., -3.])
 
-    x = torch.tensor([0.])
+    # x = torch.tensor([0.])
+    x = torch.zeros(1, 1)
 
     sampler = TruncatedGaussianSampler(A, b)
 
-    samples = []
+    for j in range(200):
+        # print("=== iter {:d} ===".format(j))
+        x = sampler.next(x)
 
-    for i in range(400):
-        for j in range(100):
-            x = sampler.next(x)
-
-        samples.append(x)
-
-    samples = torch.cat(samples, dim=-1)
-    mean = samples.mean()
-    std = samples.var()
+    samples = x
+    mean = samples.mean(dim=-2)
+    std = samples.var(dim=-2)
 
     print(mean, std)
 
@@ -127,15 +153,15 @@ if __name__ == "__main__":
     mean, var = truncnorm.stats(-1, 3)
     print(mean, var)
 
-    import matplotlib.pyplot as plt
-    plt.hist(samples.tolist(), density=True, bins=20)
-    # plt.hist(samples.tolist(), cumulative=True, density=True, bins=20)
+    # import matplotlib.pyplot as plt
+    # plt.hist(samples.tolist(), density=True, bins=20)
+    # # plt.hist(samples.tolist(), cumulative=True, density=True, bins=20)
 
-    xx = np.linspace(-1, 3.)
-    # plt.plot(xx, truncnorm.cdf(xx, -1, 3), label='truncated Gaussian CDF')
-    plt.plot(xx, truncnorm.pdf(xx, -1, 3), label='truncated Gaussian PDF')
-    plt.legend()
+    # xx = np.linspace(-1, 3.)
+    # # plt.plot(xx, truncnorm.cdf(xx, -1, 3), label='truncated Gaussian CDF')
+    # plt.plot(xx, truncnorm.pdf(xx, -1, 3), label='truncated Gaussian PDF')
+    # plt.legend()
 
-    # plt.title('cumulative histogram of MCMC samples')
-    plt.title('density histogram of MCMC samples')
-    plt.show()
+    # # plt.title('cumulative histogram of MCMC samples')
+    # plt.title('density histogram of MCMC samples')
+    # plt.show()

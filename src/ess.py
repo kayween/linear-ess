940054502eb7aed76ab8709349808f741b190e6e
Kaiwen Wu <kwwu2015@gmail.com>
2024-06-14 18:43:59 -0400

    refactor; add thinning and burnin

--- src/ess.py
+++ src/ess.py
@@ -1,92 +1,64 @@
 import math
-
-import numpy as np
 import torch
 
 import warnings
 
-import time
-
 
 class EllipticalSliceSampler(object):
-    def __init__(self, A, b, x, z):
+    def __init__(self, A, b, x, burnin=0):
         """
-        Linear inequality constraints
-            A @ x + b <= 0.
+        Elliptical slice sampler for multivariate standard Gaussian distributions under
+        linear inequality constraints A @ x <= b.
 
         Args:
-            A (tensor): the matrix of size (m, d) representing the constraint
-            b (tensor): the vector of size (d,) representing the constraint
-            x (tensor): a point on ellipse of size (*, d). Has to satisfy the linear inequality constraints
-            z (tensor): another point on the ellipse of size (*, d)
+            A (tensor): The matrix of size (m, d) in the linear inequality constraints A @ x <= b.
+            b (tensor): The vector of size (d,) in the constraints.
+            x (tensor): The initialization of size (*, d). Has to satisfy all linear inequality constraints.
+                Its first dimension is the batch size, which will be used as the number of independent Markov chains.
         """
+        if (x @ A.T - b).gt(0.).any():
+            raise RuntimeError
+
         self.A = A
         self.b = b
 
         self.x = x = x.unsqueeze(-2) if x.dim() == 1 else x
-        self.z = z = z.unsqueeze(-2) if z.dim() == 1 else z
 
-        if (x @ A.T - b).gt(0.).any():
-            raise RuntimeError
-
-        # Some common tensors that will be used more than once
+        # Some common tensors that will be used later
         batch = x.size(-2)
+
         self.zeros = x.new_zeros((batch, 1))
         self.ones = x.new_ones((batch, 1))
         self.indices_batch = torch.arange(batch, dtype=torch.int64, device=x.device)
 
-        alpha, beta = self.intersection_angles(x @ A.T, z @ A.T, b.unsqueeze(-2))
-        # alpha, beta = self.intersection_angles(
-        #     (x @ A.T).to(torch.float64),
-        #     (z @ A.T).to(torch.float64),
-        #     b.unsqueeze(-2).to(torch.float64),
-        # )
+        # Count the total number of fallback.
+        self.cnt_violations = 0
+        self.total_iterations = 0
 
-        left, right = self.active_angles(alpha, beta)
-        # self.left = self.left.to(torch.float64)
-        # self.right = self.right.to(torch.float64)
+        # Let's burn some samples.
+        self.launch(burnin)
 
-        self.left, self.right = self.postprocess_active_angles(left, right)
-
-        # self.csum = self.right.sub(self.left).clamp(min=0.).to(torch.float64).cumsum(dim=-1)
-        self.csum = self.right.sub(self.left).clamp(min=0.).cumsum(dim=-1)
-
-    def postprocess_active_angles(self, left, right):
+    def trim_active_angles(self, left, right):
+        """
+        Trim the active slices (by at most 1e-6) to ensure the samples stay inside the domain.
+        """
         gap = torch.clamp(right - left, min=0.)
+        eps = gap.mul(0.25).clamp(max=1e-6)
 
-        epsilon = torch.tensor(1e-6, dtype=left.dtype, device=left.device)
-        epsilon = torch.minimum(gap * 0.25, epsilon)
+        return left + eps, right - eps
 
-        left = left + epsilon
-        right = right - epsilon
+    def draw_angles(self, left, right):
+        csum = right.sub(left).clamp(min=0.).cumsum(dim=-1)
 
-        # return left.to(torch.float64), right.to(torch.float64)
-        return left, right
+        u = csum[:, -1] * torch.rand(right.size(-2), device=self.x.device)
 
-    def sample_angle(self):
-        # u = self.csum[:, -1] * torch.rand(self.right.size(-2), dtype=torch.float64, device=self.x.device)
-        u = self.csum[:, -1] * torch.rand(self.right.size(-2), device=self.x.device)
-
-        # The returned index i satisfies self.csum[i - 1] < u <= self.csum[i]
-        idx = torch.searchsorted(self.csum, u.unsqueeze(-1)).squeeze(-1)
+        # The returned index i satisfies csum[i - 1] < u <= csum[i]
+        idx = torch.searchsorted(csum, u.unsqueeze(-1)).squeeze(-1)
 
         # Do a zero padding so that padded_csum[i] = csum[i - 1]
-        # padded_csum = torch.cat([self.zeros.to(torch.float64), self.csum], dim=-1)
-        padded_csum = torch.cat([self.zeros, self.csum], dim=-1)
+        padded_csum = torch.cat([self.zeros, csum], dim=-1)
 
-        theta = u - padded_csum[self.indices_batch, idx] + self.left[self.indices_batch, idx]
-
-        # def check_feasibility(theta):
-        #     point = self.x * torch.cos(theta).unsqueeze(-1) + self.z * torch.sin(theta).unsqueeze(-1)
-        #     if torch.max(point @ A.T - b).gt(0.).any():
-        #         print("fuck")
-        #         import ipdb; ipdb.set_trace()
-
-        # check_feasibility(theta.to(self.b.dtype))
-
-        return theta.to(self.b.dtype)
-        # return ret
-        # return ret
+        return u - padded_csum[self.indices_batch, idx] + left[self.indices_batch, idx]
 
     def sample_slice(self):
         theta = self.sample_angle()
@@ -105,16 +77,14 @@ class EllipticalSliceSampler(object):
         """
         radius = torch.sqrt(p ** 2 + q ** 2)
 
-        if radius.abs().lt(1e-10).any():
-            raise RuntimeError
+        if radius.abs().lt(1e-6).any():
+            warnings.warn("The ellipse has an extremely small volume. This may cause numerical issues.")
 
         # It's impossible that the ratio < -1 if A @ x <= b.
         assert bias.div(radius).ge(-1).all()
 
         if bias.div(radius).min().le(-1 + 1e-6):
-            warnings.warn(
-                "The ellipse is almost outside the constraint. This may cause numerical issues."
-            )
+            warnings.warn("The ellipse is almost outside the domain. This may cause numerical issues.")
 
         has_solution = bias.div(radius).lt(1.)
 
@@ -137,10 +107,6 @@ class EllipticalSliceSampler(object):
 
         alpha, beta = torch.minimum(theta1, theta2), torch.maximum(theta1, theta2)
 
-        # return alpha, beta 
-        alpha = alpha - torch.minimum(alpha * 0.01, torch.ones_like(alpha) * 1e-5)
-        beta = beta + torch.minimum((2 * math.pi - beta) * 0.01, torch.ones_like(beta) * 1e-5)
-
         return alpha, beta
 
     def active_angles(self, alpha, beta):
@@ -161,93 +127,41 @@ class EllipticalSliceSampler(object):
 
         srted = torch.cat([srted, self.ones * 2 * math.pi], dim=-1)
         cummax = torch.cat([self.zeros, cummax], dim=-1)
- 
+
         return cummax, srted
 
-
-class TruncatedGaussianSampler(object):
-    def __init__(self, A, b):
-        self.A = A
-        self.b = b
-
-        self.cnt_warnings = 0
-
-    def next(self, x):
+    def step(self, x):
+        """
+        Do one step elliptical slice sampling. Fall back to previous state if the samples violate the constraint.
+        Each iteration has three matrix-vector multiplications. Can be reduced to two if caching A @ x.
+        """
         nu = torch.randn(x.size(), dtype=x.dtype, device=x.device)
 
-        sampler = EllipticalSliceSampler(self.A, self.b, x, nu)
+        alpha, beta = self.intersection_angles(x @ self.A.T, nu @ self.A.T, self.b.unsqueeze(-2))
+        left, right = self.active_angles(alpha, beta)
+        left, right = self.trim_active_angles(left, right)
 
-        new_x =  sampler.sample_slice()
+        theta = self.draw_angles(left, right).unsqueeze(-1)
+        candidate = x * torch.cos(theta) + nu * torch.sin(theta)
 
-        if torch.max(new_x @ A.T - b).max().ge(0.):
-            warnings.warn("sample violates the constraint")
-            self.cnt_warnings += 1
+        if torch.max(candidate @ self.A.T - self.b).ge(0.):
+            warnings.warn("Fall back to previous iteration because some constraints are violated.")
+            self.cnt_violations += 1
+        else:
+            x = candidate
 
-            new_x = x
+        return x
 
-        return new_x
+    def launch(self, num_steps, thinning=None):
+        samples = []
 
+        for i in range(num_steps):
+            self.x = self.step(self.x)
 
-if __name__ == "__main__":
-    device = "cuda:0"
+            if thinning is not None and i % thinning == 0:
+                samples.append(self.x.clone().detach())
 
-    # torch.set_default_dtype(torch.float64)
-    torch.backends.cuda.matmul.allow_tf32 = False
-    torch.backends.cudnn.allow_tf32 = False
-
-    np.random.seed(0)
-    torch.manual_seed(0)
-
-    # domain is -1 <= x <= 3
-    A = torch.tensor([[-1.], [1.]], device=device)
-    b = torch.tensor([1., 3.], device=device)
-    x = torch.zeros(10000, 1, device=device)
-
-    # m = 20
-    # d = 10
-
-    # A = torch.randn(m, d, device=device)
-    # x = torch.randn(1000, d, device=device)
-
-    # prod = x @ A.T
-    # b = prod.max(dim=-2).values + torch.rand(m, device=device)
-
-    sampler = TruncatedGaussianSampler(A, b)
-
-    start = time.time()
-    for i in range(1000):
-        # print("=== iter {:d} ===".format(i))
-        # if i == 1400:
-        #     # print(time.time() - start)
-        #     break
-        x = sampler.next(x)
-
-    print(sampler.cnt_warnings)
-    print("finish sampling")
-    print(time.time() - start)
-
-    samples = x
-    mean = samples.mean(dim=-2)
-    std = samples.var(dim=-2)
-
-    print(mean, std)
-
-    from scipy.stats import truncnorm
-    mean, var = truncnorm.stats(-1, 3)
-    print(mean, var)
-
-    # samples = samples.squeeze()
-
-    # print(samples.max())
-    # import matplotlib.pyplot as plt
-    # # plt.hist(samples.tolist(), density=True, bins='auto')
-    # plt.hist(samples.tolist(), cumulative=True, density=True, bins='auto')
-
-    # xx = np.linspace(-1, 3.)
-    # # plt.plot(xx, truncnorm.pdf(xx, -1, 3), label='truncated Gaussian PDF')
-    # plt.plot(xx, truncnorm.cdf(xx, -1, 3), label='truncated Gaussian CDF')
-    # plt.legend()
-
-    # # plt.title('cumulative histogram of MCMC samples')
-    # plt.title('density histogram of MCMC samples')
-    # plt.show()
+        if thinning is not None:
+            return torch.cat(samples, dim=-2)
+        else:
+            return self.x

3d36a2f67063a4c11ec1eba0b3472089ea21dbcf
Kaiwen Wu <kwwu2015@gmail.com>
2024-07-14 18:33:09 -0400

    add running time for parallel chains

--- plots/plot_running_time.py
+++ plots/plot_running_time.py
@@ -1,3 +1,4 @@
+import sys
 import math
 
 import torch
@@ -9,52 +10,42 @@ from botorch.utils.probability.lin_ess import \
 import time
 import matplotlib.pyplot as plt
 
-
 if __name__ == "__main__":
     torch.manual_seed(0)
 
-    # device = "cpu"
+    device = "cpu" if sys.argv[1] == "cpu" else "cuda:0"
 
-    # d = 100
-    # lst_num_constraints = [100, 500, 1000, 1500, 2000, 2500, 3000]
+    print(device)
 
-    device = "cuda:0"
-    # torch.set_default_dtype(torch.float64)
+    if device == "cpu":
+        lst_dims = [200, 400, 600, 800, 1000, 1200, 1600, 1800, 2000]
+        # lst_dims = [200, 400, 600, 800, 1000]
+    else:
+        lst_dims = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
 
+    # warm up
     mat = torch.eye(20000, device="cuda:0")
     mat = torch.sum(mat @ mat)
 
-    d = 1000
-    lst_num_constraints = [1000, 1000, 2000, 3000, 4000, 5000]
-
-    lst_time_ess = []
     lst_time_botorch = []
+    lst_time_ess = []
+    lst_time_parallel_ess = []
 
-    for m in lst_num_constraints:
+    for d in lst_dims:
+        print("****** dimension = {:d} ******".format(d))
+
+        m = d
         A = torch.randn(m, d, device=device) / math.sqrt(m)
         x = torch.randn(1, d, device=device) / math.sqrt(d)
 
         prod = x @ A.T
-        b = prod.max(dim=-2).values + torch.rand(m, device=device)
+        b = prod.squeeze() + torch.rand(m, device=device)
 
-        num_steps = n = 1000
-
-        # Let's start sampling
-        start = time.time()
-
-        sampler = EllipticalSliceSampler(A, b, x, burnin=0)
-        samples = sampler.launch(num_steps=num_steps, thinning=None)
-
-        end = time.time()
-
-        print("running time {:.4f}".format(end - start))
-        print("count violations {:d}".format(sampler.cnt_violations))
-
-        lst_time_ess.append(end - start)
+        # number of samples
+        n = 1000
 
         # BoTorch's ess
         start = time.time()
-
         botorch_sampler = BotorchEllipticalSliceSampler(
             inequality_constraints=(A, b.unsqueeze(-1)),
             interior_point=x.transpose(-1, -2),
@@ -63,19 +54,45 @@ if __name__ == "__main__":
             thinning=0,
         )
         botorch_sampler.draw(n=n)
-
         end = time.time()
 
         print("botorch running time {:.4f}".format(end - start))
-
         lst_time_botorch.append(end - start)
 
+        # Our ess
+        start = time.time()
+        sampler = EllipticalSliceSampler(A, b, x, burnin=0)
+        samples = sampler.launch(num_steps=n, thinning=None)
+        end = time.time()
+
+        print(
+            "a single chain running time {:.4f},".format(end - start),
+            "cnt violations {:d}".format(sampler.cnt_violations),
+        )
+        lst_time_ess.append(end - start)
+
+        # Our ess with 10 chains running in parallel
+        start = time.time()
+        sampler = EllipticalSliceSampler(
+            A, b, x.expand(10, -1).clone(), burnin=0,
+        )
+        samples = sampler.launch(num_steps=n // 10, thinning=None)
+        end = time.time()
+
+        print(
+            "10 chains running time {:.4f},".format(end - start),
+            "cnt violations {:d}".format(sampler.cnt_violations),
+        )
+        lst_time_parallel_ess.append(end - start)
+
     plt.figure(figsize=(3, 2.5))
-    plt.plot(lst_num_constraints[1:], lst_time_ess[1:], marker='^', label='our ess')
-    plt.plot(lst_num_constraints[1:], lst_time_botorch[1:], marker='s', label='botorch ess')
+    plt.plot(lst_dims, lst_time_botorch, marker='s', label='botorch ess')
+    plt.plot(lst_dims, lst_time_ess, marker='^', label='ours (1 chain)')
+    plt.plot(lst_dims, lst_time_parallel_ess, marker='^', label='ours (10 chains)')
     plt.title('CPU Running Time' if device == "cpu" else "GPU Running Time")
     plt.ylabel('time (s)')
-    plt.xlabel('num. of constraints')
+    plt.xlabel('num. of dimensions')
     plt.legend()
-    plt.tight_layout()
+    plt.yscale('log')
+    plt.tight_layout(pad=0)
     plt.show()

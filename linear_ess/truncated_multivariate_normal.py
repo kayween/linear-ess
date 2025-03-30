cb38a80496c10493cae371d9f5554414aad1ffb8
Kaiwen Wu <kwwu2015@gmail.com>
2025-03-30 02:42:58 -0400

    tweak arguments; add scale_tril

--- linear_ess/truncated_multivariate_normal.py
+++ linear_ess/truncated_multivariate_normal.py
@@ -3,22 +3,26 @@ from .ess import EllipticalSliceSampler
 
 
 class TruncatedMultivariateNormal:
-    def __init__(self, mean, covariance_matrix, A, b):
-        self.mean = mean
-        self.covariance_matrix = covariance_matrix
-
-        self.covar_root = torch.linalg.cholesky(covariance_matrix)
-
+    def __init__(self, A, b, loc, covariance_matrix=None, scale_tril=None):
         self.A = A
         self.b = b
 
+        self.loc = loc
+
+        if scale_tril is not None:
+            self.scale_tril = scale_tril
+        elif covariance_matrix is not None:
+            self.scale_tril = torch.linalg.cholesky(covariance_matrix)
+        else:
+            raise RuntimeError
+
     def sample(self, num_steps, burnin=100, thinning=10):
         sampler = EllipticalSliceSampler(
-            A=self.A @ self.covar_root,
-            b=self.b - self.A @ self.mean,
+            A=self.A @ self.scale_tril,
+            b=self.b - self.A @ self.loc,
             burnin=burnin,
         )
 
         samples = sampler.launch(num_steps=num_steps, thinning=thinning)
 
-        return self.mean + samples @ self.covar_root.T
+        return self.loc + samples @ self.scale_tril.T
